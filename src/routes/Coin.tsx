9dd52c2e476ae1c096e8ebe6fd4534e16dfc6b87
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-02-21 16:14:10 +0900

    feat: state을 사용해서 api를 통해 fetch가 아닌 state을 통해 직접 정보를 전달해 로드 타임 줄이기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,49 @@
-import { useParams } from "react-router";
+import {useParams} from "react-router";
+import styled from "styled-components";
+import {useState} from "react";
+import {useLocation} from "react-router-dom";
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 15vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 interface RouteParams {
-  coinId: string;
+    coinId: string;
+}
+interface RouteState{
+    name:string
+}
+function Coin() {
+    const [loading, setLoading] = useState(true);
+    const {coinId} = useParams<RouteParams>();
+    const { state } = useLocation<RouteState>();
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading..."}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
+    );
 }
 
-function Coin() {
-  const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
-}
 export default Coin;
